dc7b35cb29a6422050145d611ff7fc9b61e8102e
urikabupt <urika.bupt@foxmail.com>
2019-12-09 22:50:08 +0800

    新项目提交

---
+++ src/day01_20191102/ABC.java
@@ -0,0 +1,10 @@
+package day01_20191102;
+
+public class ABC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/day01_20191102/HelloWorld.java
@@ -0,0 +1,11 @@
+package day01_20191102;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello, world!");
+
+	}
+
+}
